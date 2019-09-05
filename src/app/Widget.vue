8ea00ce8331c1d59af4ad939583d207d5a89ee0b
Jakub Niewczas <niewczas.jakub@gmail.com>
2019-09-05 23:42:48 +0900

    basic demo project setup

---
+++ src/app/Widget.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="widget">
+    <p class="text">this text should be purple according to local css</p>
+    <a class="externallyStyledA">this text should be left unstyled because there are no local styles</a>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style lang="less" scoped>
+.text {
+  color: purple;
+}
+</style>
