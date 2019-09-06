114a64b79a9acde819aec6e09667dd2f0f6a341a
Jakub Niewczas <niewczas.jakub@gmail.com>
2019-09-06 21:44:16 +0900

    PoC of postcss-initial - part 2 - reseting all rules within widget
    all widget styles are correctly reset to its initial values, both defined as well as those left unstyled by the app

--- src/app/Widget.vue
+++ src/app/Widget.vue
@@ -8,9 +8,30 @@
 <script>
 </script>
 
+<style lang="less">
+//
+// global .widget narrowed css reset. Can be pleased in a separate file.
+.widget {
+  div, span, applet, object, iframe,
+  h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+  a, abbr, acronym, address, big, cite, code,
+  del, dfn, em, img, ins, kbd, q, s, samp,
+  small, strike, strong, sub, sup, tt, var,
+  b, u, i, center,
+  dl, dt, dd, ol, ul, li,
+  fieldset, form, label, legend,
+  table, caption, tbody, tfoot, thead, tr, th, td,
+  article, aside, canvas, details, embed,
+  figure, figcaption, footer, header, hgroup,
+  menu, nav, output, ruby, section, summary,
+  time, mark, audio, video {
+    all: initial;
+  }
+}
+</style>
+
 <style lang="less" scoped>
 .text {
-  all: initial;
   color: purple;
 }
 </style>
